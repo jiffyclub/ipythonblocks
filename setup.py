ebecbc55c5b4fa01052e380d76be10e3acd73717
Matt Davis <jiffyclub@gmail.com>
2013-01-11 20:15:36 -0500

    Post release version number bump to 1.2dev.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='ipythonblocks',
-      version='1.1',
+      version='1.2dev',
       description='Practice Python with colored grids in the IPython Notebook',
       author='Matt Davis',
       author_email='jiffyclub@gmail.com',
