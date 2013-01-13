cfe0ab2f2aad9470498c51775ff4ce3c26c9add3
Matt Davis <jiffyclub@gmail.com>
2013-01-12 21:45:27 -0500

    Adding unit tests.

---
+++ ipythonblocks/test/test_block.py
@@ -0,0 +1,149 @@
+import os
+import pytest
+
+from .. import ipythonblocks
+
+
+@pytest.fixture
+def basic_block():
+    return ipythonblocks.Block(5, 6, 7, size=20)
+
+
+def test_basic_api(basic_block):
+    """
+    Check that inputs are going to the right attributes and that assignment
+    works when it should and not when it shouldn't.
+
+    """
+    bb = basic_block
+
+    assert bb.colors == (5, 6, 7)
+
+    assert bb.red == 5
+    bb.red = 1
+    assert bb.red == 1
+
+    assert bb.green == 6
+    bb.green = 2
+    assert bb.green == 2
+
+    assert bb.blue == 7
+    bb.blue = 3
+    assert bb.blue == 3
+
+    assert bb.colors == (1, 2, 3)
+
+    assert bb.size == 20
+    bb.size = 10
+    assert bb.size == 10
+
+    assert bb.row is None
+    with pytest.raises(AttributeError):
+        bb.row = 5
+
+    assert bb.col is None
+    with pytest.raises(AttributeError):
+        bb.col = 5
+
+
+def test_attribute_limits(basic_block):
+    """
+    Color and size attributes have some builtin limits, test that they
+    are respected.
+
+    """
+    bb = basic_block
+
+    bb.red = -50
+    assert bb.red == 0
+
+    bb.green = 1000
+    assert bb.green == 255
+
+    bb.size = -200
+    assert bb.size == ipythonblocks._SMALLEST_BLOCK
+
+
+def test_check_value(basic_block):
+    """
+    Test the Block._check_value method that enforces color limits,
+    converts to int, and checks values are numbers.
+
+    """
+    bb = basic_block
+
+    bb.red = 4.56
+    assert isinstance(bb.red, int)
+    assert bb.red == 5
+
+    bb.blue = 200.1
+    assert isinstance(bb.blue, int)
+    assert bb.blue == 200
+
+    with pytest.raises(ipythonblocks.InvalidColorSpec):
+        bb.green = 'green'
+
+
+def test_set_colors(basic_block):
+    """
+    Test the Block.set_colors method.
+
+    """
+    bb = basic_block
+
+    bb.set_colors(200, 201, 202)
+
+    assert bb.red == 200
+    assert bb.green == 201
+    assert bb.blue == 202
+
+
+def test_td(basic_block):
+    """
+    Test the Block.td proerty that returns an HTML table cell.
+
+    """
+    bb = basic_block
+
+    rgb = ipythonblocks._RGB.format(bb.red, bb.green, bb.blue)
+    td = ipythonblocks._TD.format(bb.size, rgb)
+
+    assert bb.td == td
+
+
+def test_repr_html(basic_block):
+    """
+    Test the Block._repr_html_ method that returns a single cell HTML table.
+
+    """
+    bb = basic_block
+
+    table = ipythonblocks._TABLE.format(ipythonblocks._TR.format(bb.td))
+
+    assert bb._repr_html_() == table
+
+
+def test_str1(basic_block):
+    """
+    Test the Block.__str__ method used with print.
+
+    """
+    bb = basic_block
+
+    s = os.linesep.join(['Block', 'Color: (5, 6, 7)'])
+
+    assert bb.__str__() == s
+
+
+def test_str2(basic_block):
+    """
+    Test the Block.__str__ method used with print.
+
+    """
+    bb = basic_block
+    bb._row = 8
+    bb._col = 9
+
+    s = os.linesep.join(['Block (8, 9)', 'Color: (5, 6, 7)'])
+
+    assert bb.__str__() == s
---
+++ ipythonblocks/test/test_blockgrid.py
@@ -0,0 +1,269 @@
+import os
+import pytest
+
+from .. import ipythonblocks
+
+
+@pytest.fixture
+def basic_grid():
+    return ipythonblocks.BlockGrid(5, 6, (1, 2, 3), 20)
+
+
+def test_basic_api(basic_grid):
+    """
+    Check that inputs are going to the right attributes and that assignment
+    works when it should and not when it shouldn't.
+
+    """
+    bg = basic_grid
+
+    assert bg.width == 5
+    with pytest.raises(AttributeError):
+        bg.width = 20
+
+    assert bg.height == 6
+    with pytest.raises(AttributeError):
+        bg.height = 20
+
+    assert bg.shape == (5, 6)
+    assert bg.block_size == 20
+
+
+def test_grid_init(basic_grid):
+    """
+    Test that the grid is properly initialized.
+
+    """
+    bg = basic_grid
+
+    for r in range(bg.height):
+        for c in range(bg.width):
+            assert bg[r, c].size == 20
+            assert bg[r, c].red == 1
+            assert bg[r, c].green == 2
+            assert bg[r, c].blue == 3
+            assert bg[r, c].row == r
+            assert bg[r, c].col == c
+
+
+def test_change_block_size(basic_grid):
+    """
+    Test that all blocks are properly resized when changing the
+    BlockGrid.block_size attribute.
+
+    """
+    bg = basic_grid
+
+    bg.block_size = 10
+    assert bg.block_size == 10
+
+    for block in bg:
+        assert block.size == 10
+
+
+def test_view(basic_grid):
+    """
+    Check that getting a new BlockGrid object via slicing returns a view
+    and not a copy.
+
+    """
+    bg = basic_grid
+    ng = bg[:2, :2]
+
+    ng[1, 1].set_colors(200, 201, 202)
+
+    for block in (ng[1, 1], bg[1, 1]):
+        assert block.red == 200
+        assert block.green == 201
+        assert block.blue == 202
+
+
+def test_view_coords(basic_grid):
+    """
+    Make sure that when we get a view that it has its own appropriate
+    coordinates.
+
+    """
+    ng = basic_grid[-2:, -2:]
+
+    coords = ((0, 0), (0, 1), (1, 0), (1, 1))
+
+    for b, c in zip(ng, coords):
+        assert b.row == c[0]
+        assert b.col == c[1]
+
+
+def test_copy(basic_grid):
+    """
+    Check that getting a new BlockGrid via BlockGrid.copy returns a totally
+    independent copy and not a view.
+
+    """
+    bg = basic_grid
+    ng = bg[:2, :2].copy()
+
+    ng[1, 1].set_colors(200, 201, 202)
+
+    assert ng[1, 1].red == 200
+    assert ng[1, 1].green == 201
+    assert ng[1, 1].blue == 202
+    assert bg[1, 1].red == 1
+    assert bg[1, 1].green == 2
+    assert bg[1, 1].blue == 3
+
+
+def test_str(basic_grid):
+    """
+    Test the BlockGrid.__str__ method used with print.
+
+    """
+    bg = basic_grid
+
+    s = os.linesep.join(['BlockGrid', 'Shape: (5, 6)'])
+
+    assert bg.__str__() == s
+
+
+def test_repr_html():
+    """
+    HTML repr should be the same for a 1, 1 BlockGrid as for a single Block.
+
+    """
+    bg = ipythonblocks.BlockGrid(1, 1)
+
+    assert bg._repr_html_() == bg[0, 0]._repr_html_()
+
+
+def test_iter():
+    """
+    Test that we do complete, row first iteration.
+
+    """
+    bg = ipythonblocks.BlockGrid(2, 2)
+
+    coords = ((0, 0), (0, 1), (1, 0), (1, 1))
+
+    for b, c in zip(bg, coords):
+        assert b.row == c[0]
+        assert b.col == c[1]
+
+
+def test_bad_index(basic_grid):
+    """
+    Test for the correct errors with bad indices.
+
+    """
+    bg = basic_grid
+
+    with pytest.raises(IndexError):
+        bg[1, 2, 3, 4]
+
+    with pytest.raises(IndexError):
+        bg[{4: 5}]
+
+    with pytest.raises(TypeError):
+        bg[1, ]
+
+
+def test_bad_colors(basic_grid):
+    """
+    Make sure this gets the right error when trying to assign something
+    other than three integers.
+
+    """
+    with pytest.raises(ValueError):
+        basic_grid[0, 0] = (1, 2, 3, 4)
+
+
+def test_getitem(basic_grid):
+    """
+    Exercise a bunch of different indexing.
+
+    """
+    bg = basic_grid
+
+    # single block
+    block = bg[1, 2]
+
+    assert isinstance(block, ipythonblocks.Block)
+    assert block.row == 1
+    assert block.col == 2
+
+    # single row
+    ng = bg[2]
+
+    assert isinstance(ng, ipythonblocks.BlockGrid)
+    assert ng.shape == (bg.width, 1)
+
+    # two rows
+    ng = bg[1:3]
+
+    assert isinstance(ng, ipythonblocks.BlockGrid)
+    assert ng.shape == (bg.width, 2)
+
+    # one row via a slice
+    ng = bg[2, :]
+
+    assert isinstance(ng, ipythonblocks.BlockGrid)
+    assert ng.shape == (bg.width, 1)
+
+    # one column
+    ng = bg[:, 2]
+
+    assert isinstance(ng, ipythonblocks.BlockGrid)
+    assert ng.shape == (1, bg.height)
+
+    # 2 x 2 subgrid
+    ng = bg[:2, :2]
+
+    assert isinstance(ng, ipythonblocks.BlockGrid)
+    assert ng.shape == (2, 2)
+
+    # strided slicing
+    ng = bg[::3, ::3]
+
+    assert isinstance(ng, ipythonblocks.BlockGrid)
+    assert ng.shape == (2, 2)
+
+
+def test_setitem(basic_grid):
+    """
+    Test assigning colors to blocks.
+
+    """
+    bg = basic_grid
+    colors = (21, 22, 23)
+
+    # single block
+    bg[0, 0] = colors
+    assert bg[0, 0].colors == colors
+
+    # single row
+    bg[2] = colors
+    for block in bg[2]:
+        assert block.colors == colors
+
+    # two rows
+    bg[3:5] = colors
+    for block in bg[3:5]:
+        assert block.colors == colors
+
+    # one row via a slice
+    bg[1, :] = colors
+    for block in bg[1, :]:
+        assert block.colors == colors
+
+    # one column
+    bg[:, 5] = colors
+    for block in bg[:, 5]:
+        assert block.colors == colors
+
+    # 2 x 2 subgrid
+    bg[:2, :2] = colors
+    for block in bg[:2, :2]:
+        assert block.colors == colors
+
+    # strided slicing
+    bg[::3, ::3] = colors
+    for block in bg[::3, ::3]:
+        assert block.colors == colors
