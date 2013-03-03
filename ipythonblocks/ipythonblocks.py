a3bfc2c75b859ad65a6dc5a8c68d6517a7df4454
Matt Davis <jiffyclub@gmail.com>
2013-03-03 18:05:03 -0500

    Post-release version number bump.

--- ipythonblocks/ipythonblocks.py
+++ ipythonblocks/ipythonblocks.py
@@ -27,7 +27,7 @@ if sys.version_info[0] >= 3:
 __all__ = ('Block', 'BlockGrid', 'Pixel', 'ImageGrid',
            'InvalidColorSpec', 'show_color', 'embed_colorpicker',
            'colors', '__version__')
-__version__ = '1.5'
+__version__ = '1.6dev'
 
 _TABLE = ('<style type="text/css">'
           'table.blockgrid {{border: none;}}'
