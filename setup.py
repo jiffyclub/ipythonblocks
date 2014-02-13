75d38ae56eccf50fe08bf1b4e6f8d3e323257e9c
Matt Davis <jiffyclub@gmail.com>
2014-02-13 14:19:07 -0800

    Switch to setuptools so we can list requests as a requirement.

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
+from ez_setup import use_setuptools
+use_setuptools()
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+  long_description=f.read()
 
 setup(name='ipythonblocks',
       version='1.6dev',
       description='Practice Python with colored grids in the IPython Notebook',
+      long_description=long_description,
       author='Matt Davis',
       author_email='jiffyclub@gmail.com',
       url='https://github.com/jiffyclub/ipythonblocks/',
-      packages=['ipythonblocks'],
+      packages=find_packages(exclude=['*.test']),
       classifiers=['License :: OSI Approved :: MIT License',
                    'Programming Language :: Python',
                    'Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 3',
                    'Intended Audience :: Education',
-                   'Topic :: Education'])
+                   'Topic :: Education'],
+      install_requires=['requests>=1.0'])
